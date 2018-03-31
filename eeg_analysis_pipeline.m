e555b38d9400375f8ef31576cc93f0452d93b3b1
mariellapaul <37513037+mariellapaul@users.noreply.github.com>
2018-03-31 15:26:20 +0200

    Add files via upload

--- eeg_analysis_pipeline.m
+++ eeg_analysis_pipeline.m
@@ -1,10 +1,10 @@
 clear variables;
 
 % define variables with path names
-dir_main = 'C:\Users\Mariella\Desktop\Mind & Brain\EEG Kurs\';
-dir_data = [dir_main 'Data\']; % 
+dir_main = 'D:\Mind & Brain\EEG Kurs\';
+dir_data = [dir_main 'EEG Daten Antonia\']; % 
 % assure fieldtrip is in path
-addpath('C:\Users\Mariella\Desktop\Crossing Borders\fieldtrip-20170105');
+addpath('D:\Mind & Brain\EEG Kurs\fieldtrip-20180328');
 ft_defaults;
 
  % matrix with subject names
@@ -21,23 +21,20 @@ subject = name_subj{1};
 headerfile   = [dir_data subject '.vhdr'];
 eegfile     = [dir_data subject '.eeg'];
 
-%% PREPROCESSING
+%% preprocessing
 
 % read data in
 cfg = [];
-cfg.dataset = [dir_data subject '.vhdr'];
+cfg.dataset = headerfile;
 data_raw = ft_preprocessing(cfg);
 
 % plot raw data
 % plot(data_raw.time{1}, data_raw.trial{1}(:,:))
 
-% rereference to linked mastoids
-% REF is the electrode on the left mastoid, the online reference
-% RM is the electrode on the right mastoid
+% rereference to M2
 cfg = [];
 cfg.reref = 'yes';
-cfg.implicitref = 'REF';
-cfg.refchannel = {'RM' 'REF'};
+cfg.refchannel = {'M2'};
 data_reref = ft_preprocessing(cfg, data_raw);
 
 % % plot re-referenced data
@@ -47,11 +44,15 @@ data_reref = ft_preprocessing(cfg, data_raw);
 %% exercise
 
 % % try different high-pass filters and plot the data
-% cutoff_hp = [0.001 0.1 1 10];
+% cutoff_hp = [0.1 1 10];
 % cfg = [];
 % cfg.hpfilter = 'yes';
+% cfg.hpfilttype = 'firws';
+% cfg.hpfiltdir = 'onepass-zerophase';
+% cfg.hpinstabilityfix = 'no';
 % for i = 1:length(cutoff_hp)
 % 	cfg.hpfreq = cutoff_hp(i);
+%     cfg.hpfiltdf = cfg.hpfreq;
 % 	data_hp = ft_preprocessing(cfg, data_reref);
 % 	figure;
 % 	plot(data_hp.time{1}, data_hp.trial{1}(:,:))
@@ -61,6 +62,10 @@ data_reref = ft_preprocessing(cfg, data_raw);
 % cutoff_lp = [10 20 30 100];
 % cfg = [];
 % cfg.lpfilter = 'yes';
+% cfg.hpfilttype = 'firws';
+% cfg.hpfiltdir = 'onepass-zerophase';
+% cfg.hpinstabilityfix = 'no';
+% cfg.hpfiltdf = 5;
 % for i = 1:length(cutoff_lp)
 % 	cfg.lpfreq = cutoff_lp(i);
 % 	data_lp = ft_preprocessing(cfg, data_reref);
@@ -68,28 +73,36 @@ data_reref = ft_preprocessing(cfg, data_raw);
 % 	plot(data_lp.time{1}, data_lp.trial{1}(:,1))
 % end
 
-%%
+%% preprocessing (continued)
 
 % high-pass filter with the "final" options
-% cfg = [];
-% cfg.hpfilter = 'yes';
-% cfg.hpfreq = 0.1;
-% data_hp = ft_preprocessing(cfg, data_raw);
+cfg = [];
+cfg.hpfilter = 'yes';
+cfg.hpfreq = 0.1;
+cfg.hpfilttype = 'firws';
+cfg.hpfiltdir = 'onepass-zerophase';
+cfg.hpinstabilityfix = 'no';
+cfg.hpfiltwintype = 'kaiser';
+data_hp = ft_preprocessing(cfg, data_reref);
 % 
 % % plot high-pass filtered data
-% figure;
-% plot(data_hp.time{1}, data_hp.trial{1}(:,:))
+figure;
+plot(data_hp.time{1}, data_hp.trial{1}(:,:))
 
 
 % low-pass filter with the "final" options
 cfg = [];
 cfg.lpfilter = 'yes';
-cfg.lpfilter = 70;
-data_lp = ft_preprocessing(cfg, data_reref);
+cfg.lpfilter = 30;
+cfg.hpfilttype = 'firws';
+cfg.hpfiltdir = 'onepass-zerophase';
+cfg.hpinstabilityfix = 'no';
+cfg.hpfiltdf = 5;
+data_lp = ft_preprocessing(cfg, data_hp);
 
 % plot low-pass filtered data
-% figure;
-% plot(data_lp.time{1}, data_lp.trial{1}(:,:))
+figure;
+plot(data_lp.time{1}, data_lp.trial{1}(:,:))
 
 % look at triggers
 cfg = [];
@@ -100,88 +113,82 @@ ft_definetrial(cfg);
 
 % define trials
 cfg = [];
-cfg.trialfun     = ['trialfun_affcog'];
-cfg.headerfile   = headerfile;
-cfg.datafile     = eegfile;
+cfg.headerfile = headerfile;
+cfg.dataset = eegfile;
+cfg.trialdef.eventtype = 'Stimulus';
+cfg.trialdef.eventvalue = {'S115', 'S116', 'S125', 'S126', 'S135', 'S136'...
+    'S145', 'S146', 'S215', 'S216', 'S225', 'S226', 'S235', 'S236'...
+    'S245', 'S246'};
+cfg.trialdef.prestim = 0.2;
+cfg.trialdef.poststim = 1;
 cfg_deftrial = ft_definetrial(cfg);
-    
+
 % apply trial definition to data
 data_segmented = ft_redefinetrial(cfg_deftrial, data_lp);
 
-%% ARTIFACT REJECTION
+%% artifact rejection
 
-% muscle artifacts
-cfg            = [];
-% channel selection, cutoff and padding
-cfg.artfctdef.zvalue.channel     = 'all';
-cfg.artfctdef.zvalue.cutoff      = 7;
-cfg.artfctdef.zvalue.trlpadding  = 0;
-cfg.artfctdef.zvalue.fltpadding  = 0;
-cfg.artfctdef.zvalue.artpadding  = 0.1;
-% algorithmic parameters
-cfg.artfctdef.zvalue.bpfilter    = 'yes';
-cfg.artfctdef.zvalue.bpfreq      = [110 140];
-cfg.artfctdef.zvalue.bpfiltord   = 9;
-cfg.artfctdef.zvalue.bpfilttype  = 'but';
-cfg.artfctdef.zvalue.hilbert     = 'yes';
-cfg.artfctdef.zvalue.boxcar      = 0.2;
-% make the process interactive
-%cfg.artfctdef.zvalue.interactive = 'yes';
-[cfg, artifact_muscle] = ft_artifact_zvalue(cfg, data_segmented);
-
-
-% jump artifacts
-cfg            = [];   
-% channel selection, cutoff and padding
-cfg.artfctdef.zvalue.channel    = 'all';
-cfg.artfctdef.zvalue.cutoff     = 20;
-cfg.artfctdef.zvalue.trlpadding = 0;
-cfg.artfctdef.zvalue.artpadding = 0;
-cfg.artfctdef.zvalue.fltpadding = 0;
-% algorithmic parameters
-cfg.artfctdef.zvalue.cumulative    = 'yes';
-cfg.artfctdef.zvalue.medianfilter  = 'yes';
-cfg.artfctdef.zvalue.medianfiltord = 9;
-cfg.artfctdef.zvalue.absdiff       = 'yes';
-% make the process interactive
-%cfg.artfctdef.zvalue.interactive = 'yes';
-[cfg, artifact_jump] = ft_artifact_zvalue(cfg, data_segmented);
-
-% display data with artifacts highlighted
+% display data in databrowser for manual rejection
 cfg = [];
-cfg.channel = 'all';
 cfg.viewmode = 'vertical';
-cfg.ploteventlabels  = 'type=value';
-%cfg.eegscale    = 0.4;
-%cfg.mychanscale = 0.4;
-%cfg.mychan    = {'H+' , 'H-', 'V+', 'V-'};
-%checkall = ft_appenddata(cfg,data_final);
-%cfg.ylim = [-50 50];
-%cfg.channel = [1:15 17:28 32 33];
-%cfg.event = data_nounonset_event;
-cfg.artfctdef.muscle.artifact = artifact_muscle;
-cfg.artfctdef.jump.artifact = artifact_jump;
-
 artf=ft_databrowser(cfg,data_segmented);
 
- % artifact rejection: now the arifacts that were searched on the raw data
- % (muscle and jump) are removed from the epoched data
+ % artifact rejection: now the arifacts that were highlighted are removed
+ % from the data
  cfg = [];
  cfg.artfctdef.reject = 'complete';
- % rejection of automatic muscle artifacts
- cfg.artfctdef.muscle.artifact = artf.artfctdef.muscle.artifact;
- % rejection of automatic jump artifacts
- cfg.artfctdef.jump.artifact = artf.artfctdef.jump.artifact;
  % rejection of visual artifacts
  cfg.artfctdef.visual.artifact = artf.artfctdef.visual.artifact;
  
  data_clean = ft_rejectartifact(cfg,data_segmented);
- 
-  % save cleaned data
-%  save([dir_data 'data_clean'], 'data_clean');
- 
-
 
 %% ICA
+    
+% exclude bipolar EOG electrodes (not independent)
+cfg = [];
+cfg.channel = {'all' '-VEOG'};
+data_clean = ft_selectdata(cfg, data_clean);
+%this leaves 31 channels
+    
+% preprocess to obtain data format needed for ICA
+cfg = [];
+data_clean = ft_preprocessing(cfg, data_clean);
 
+% perform the independent component analysis (i.e., decompose the data)
+% ICA returns different components every time, so only do this when
+% the data has changed since the last time
+cfg        = [];
+cfg.method = 'runica'; 
+cfg.runica.pca = 30;
+% berechnet pca, schmeißt kleinsten Komponenten weg und berechnet
+% auf den übrigen ICA
+% components auf 30 Komponenten rechnen (bipolare EOG raus, dann N-1)
+comp = ft_componentanalysis(cfg, data_clean);
+
+% visualization (I): topographies
+% plot the components for visual inspection
+figure
+cfg = [];
+cfg.component = 1:30;
+cfg.layout = [dir_main 'Scripts' filesep 'layout.mat']; 
+cfg.comment = 'no';
+%cfg.style     = 'straight'; 
+%cfg.gridscale = 300;
+ft_topoplotIC(cfg, comp);
+
+% visualization (II): time representation
+% browse through components and trials
+cfg = [];
+cfg.layout = [dir_main 'Scripts' filesep 'layout.mat']; 
+cfg.viewmode = 'component';
+cfg.ylim = [-256 256];
+cfg.verticalpadding = 0.1;
+ft_databrowser(cfg, comp);
+
+% remove the bad components and backproject the data
+cfg = [];
+cfg.component = [];
+data_ica = ft_rejectcomponent(cfg, comp, data_clean);
+
+%% averaging
 
